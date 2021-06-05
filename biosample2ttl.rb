ee47759381dd5d33b8c472f47b3d0b681ded376c
Takatomo Fujisawa <fujisawa.takatomo@gmail.com>
2021-06-05 12:47:40 +0900

    cosmetic

--- biosample2ttl.rb
+++ biosample2ttl.rb
@@ -1,21 +1,5 @@
 #!/usr/bin/env ruby
-require 'rubygems'
-#require 'rdf'
-#require 'rdf/ntriples'
-#require 'rdf/turtle'
 require './lib/biosample.rb'
-require 'nokogiri'
-require 'erb'
-#include RDF
-
-#f = open(ARGV.shift)
-#f_out = open(ARGV.shift, "w")
-#r = MassBank::Record.new(f.read)
-##r.record.each do |e|
-##  p e
-##end
-#factory = MassBank::RDFFactory.new(r.record, f_out)
-#factory.rdfize
 
 xml =  ARGV.shift || 'test.xml'
 bss = DDBJ::Utils::BioSampleSet.new(xml)
